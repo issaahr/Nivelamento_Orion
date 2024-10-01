66a6e915f498f4b00c49f5cbdfa7fefbcc004901
Isabeli Romano Rodrigues <isabeli.romano@gmail.com>
2024-10-01 14:44:36 -0300

    Primeiro commit

---
+++ jsScripts/appTeste.js
@@ -0,0 +1,10 @@
+//teste para ver se o ambiente foi configurado corretamente
+var OlaTypeScript = /** @class */ (function () {
+    function OlaTypeScript(message) {
+        this.message = message;
+    }
+    return OlaTypeScript;
+}());
+var ola = new OlaTypeScript("Macoratti .net - Quase tudo para .NET!!");
+console.log(ola.message);
+//# sourceMappingURL=appTeste.js.map
