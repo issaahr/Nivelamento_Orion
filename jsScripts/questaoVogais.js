30ce84199a530ffbce12aaec82eea6acd3e5ab85
Isabeli Romano Rodrigues <isabeli.romano@gmail.com>
2024-10-03 11:35:35 -0300

    Ajuste de espaçamento, tipagem de constante e adição de documentação nas funções

--- jsScripts/questaoVogais.js
+++ jsScripts/questaoVogais.js
@@ -1,15 +1,35 @@
 "use strict";
-//Função que imprime os resultados
+/**
+ * Imprime o resultado da contagem de vogais em uma palavra
+ * @remarks
+ * Essa função exibe uma mensagem no console que mostra a palavra e o número de vogais que ela contém.
+ *
+ * @param palavra - A palavra de entrada cuja contagem será exibida
+ * @param vogaisContadas - O número de vogais contadas na palavra
+ * @returns Não retorna valor (void)
+ */
 function imprimeResultado(palavra, vogaisContadas) {
     console.log(`A palavra ${palavra} tem: ${vogaisContadas} vogais`);
 }
-/* Função que testa se a entrada recebida é do tipo string
- * ^[a-zA-ZÀ-ÿ]+$ teste regex p/ verificar se a string contem só letras
-*/
+/**
+ * Função que testa se a entrada recebida é do tipo string e é válida pelo teste regex
+ * @remarks
+ * O regex `^[a-zA-ZÀ-ÿ]+$` verifica se a string contém apenas letras, incluindo acentos.
+ * @param stringTestada - A string que será validada
+ * @returns Retorna `true` se for válida, ou `false` se for inválida
+ */
 function validaString(stringTestada) {
     return /^[a-zA-ZÀ-ÿ]+$/.test(stringTestada);
 }
-// Função que faz a contagem das vogais
+/**
+ * Conta o número de vogais em uma palavra fornecida.
+ * @remarks
+ * Esta função considera as vogais acentuadas (AÁÂÃEÉÊIÍÎOÓÔÕUÚÛ)
+ * e não diferencia entre maiúsculas e minúsculas.
+ *
+ * @param palavra - A palavra na qual contar as vogais.
+ * @returns O número de vogais na palavra fornecida.
+ */
 function contaVogais(palavra) {
     const vogaisBase = "AÁÂÃEÉÊIÍÎOÓÔÕUÚÛ";
     let contador = 0;
